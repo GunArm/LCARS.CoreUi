2ef59b49fd69b0f4bac6e33440cf28381d500dfe
Barrett Lewis <james.barrett.lewis@gmail.com>
2017-06-24 01:49:21 -0500

    Switch to embedded assets

--- LCARS.CoreUi/Assets/Access/FontFamilyProvider.cs
+++ LCARS.CoreUi/Assets/Access/FontFamilyProvider.cs
@@ -5,11 +5,14 @@ using System.Drawing;
 using System.Drawing.Text;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 
 namespace LCARS.CoreUi.Assets.Access
 {
     public static class FontFamilyProvider
     {
+        private static Assembly assembly = Assembly.GetExecutingAssembly();
+
         private static Dictionary<string, FontFamily> lcarsFontFamilies;
         private static Dictionary<string, FontFamily> alienFontFamilies;
         private static Dictionary<string, FontFamily> miscFontFamilies;
@@ -20,34 +23,66 @@ namespace LCARS.CoreUi.Assets.Access
 
         static FontFamilyProvider()
         {
-            lcarsFontFamilies = GetFontCollection("LCARS");
-            alienFontFamilies = GetFontCollection("Alien");
-            miscFontFamilies = GetFontCollection("Misc");
-        }
-
-        private static Dictionary<string, FontFamily> GetFontCollection(string fontGroup)
-        {
-            string subFolderPath = Path.Combine(Paths.FontsDir, fontGroup);
-
-            var fonts = new Dictionary<string, FontFamily>(StringComparer.InvariantCultureIgnoreCase);
-            if (!Directory.Exists(subFolderPath)) return fonts;
-
-            foreach (var fontFilePath in Directory.EnumerateFiles(subFolderPath))
+            LoadFontFamiliesFromResources(ref lcarsFontFamilies, new List<string>
             {
-                if (!fontFilePath.ToLower().EndsWith("ttf") && !fontFilePath.ToLower().EndsWith("otf")) continue;
+                "LCARS.CoreUi.Assets.Fonts.LCARS.lcars-full.ttf",
+                "LCARS.CoreUi.Assets.Fonts.LCARS.lcars-lite.ttf",
+            });
 
-                var families = ReadFontFile(fontFilePath);
-                if (families.Length != 1) throw new Exception("Font file " + fontFilePath + " has more (or less) than one font");
-                fonts.Add(Path.GetFileNameWithoutExtension(fontFilePath), families[0]);
-            }
+            LoadFontFamiliesFromResources(ref alienFontFamilies, new List<string>
+            {
+                "LCARS.CoreUi.Assets.Fonts.Alien.bajoran.ttf",
+                "LCARS.CoreUi.Assets.Fonts.Alien.cardassian.ttf",
+                "LCARS.CoreUi.Assets.Fonts.Alien.dominion.ttf",
+                "LCARS.CoreUi.Assets.Fonts.Alien.fabrini.ttf",
+                "LCARS.CoreUi.Assets.Fonts.Alien.ferengi.ttf",
+                "LCARS.CoreUi.Assets.Fonts.Alien.klingon.ttf",
+                "LCARS.CoreUi.Assets.Fonts.Alien.romulan.ttf",
+                "LCARS.CoreUi.Assets.Fonts.Alien.tholian.ttf",
+                "LCARS.CoreUi.Assets.Fonts.Alien.trill.ttf",
+                "LCARS.CoreUi.Assets.Fonts.Alien.vulcan.ttf",
+            });
 
-            return fonts;
+            LoadFontFamiliesFromResources(ref miscFontFamilies, new List<string>
+            {
+                "LCARS.CoreUi.Assets.Fonts.Misc.tng_credits.ttf",
+                "LCARS.CoreUi.Assets.Fonts.Misc.tng_monitors.ttf",
+                "LCARS.CoreUi.Assets.Fonts.Misc.tng_title.ttf",
+                "LCARS.CoreUi.Assets.Fonts.Misc.trekbats.ttf",
+            });
         }
 
-        private static FontFamily[] ReadFontFile(string fontFileName)
+        private static void LoadFontFamiliesFromResources(ref Dictionary<string, FontFamily> loadInto, List<string> namespacePaths)
+        {
+            loadInto = new Dictionary<string, FontFamily>();
+            foreach (string namespacePath in namespacePaths)
+            {
+                var bits = namespacePath.Split('.');
+                string extension = bits[bits.Length - 1];
+                if (!extension.ToLower().EndsWith("ttf") && !extension.ToLower().EndsWith("otf")) throw new Exception("This error message sucks");
+
+                var families = GetFontFamiliesFromResource(namespacePath);
+                if (families.Length != 1) throw new Exception("Font file " + namespacePath + " has more (or less) than one font");
+                string key = bits[bits.Length - 2];
+                loadInto.Add(key, families[0]);
+            }
+        }
+        
+        private static FontFamily[] GetFontFamiliesFromResource(string fontNamespacePath)
         {
             var pfc = new PrivateFontCollection();
-            pfc.AddFontFile(fontFileName);
+            Stream fontStream = assembly.GetManifestResourceStream(fontNamespacePath);
+
+            byte[] fontdata = new byte[fontStream.Length];
+            fontStream.Read(fontdata, 0, (int)fontStream.Length);
+            fontStream.Close();
+            unsafe
+            {
+                fixed (byte* pFontData = fontdata)
+                {
+                    pfc.AddMemoryFont((System.IntPtr)pFontData, fontdata.Length);
+                }
+            }
             return pfc.Families;
         }
     }
