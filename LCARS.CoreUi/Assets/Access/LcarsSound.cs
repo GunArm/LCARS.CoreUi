6bb228093e54dc6677cc9eccdb0b831909910de7
Barrett Lewis <james.barrett.lewis@gmail.com>
2017-07-01 23:45:39 -0500

    Add LcarsSound

---
+++ LCARS.CoreUi/Assets/Access/LcarsSound.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using System.Media;
+
+namespace LCARS.CoreUi.Assets.Access
+{
+    class LcarsSound
+    {
+        public static SoundPlayer soundPlayer = new SoundPlayer();
+        public static void Play(LcarsSoundAsset assetKey)
+        {
+            var resourceStream = GetGetResourceStream(assetKey);
+            if (resourceStream == null) return;
+
+            lock (soundPlayer)
+            {
+                soundPlayer.Stream = resourceStream;
+                soundPlayer.Play();
+            }
+        }
+
+        private static Stream GetGetResourceStream(LcarsSoundAsset assetKey)
+        {
+            switch (assetKey)
+            {
+                case LcarsSoundAsset.Unset:
+                    return null;
+            }
+            return null;
+        }
+    }
+
+    public enum LcarsSoundAsset
+    {
+        Unset = 0,
+    }
+}
