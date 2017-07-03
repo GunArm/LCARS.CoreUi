294e0c091e110358f0665eb4219f8f46e52b7fdf
Barrett Lewis <james.barrett.lewis@gmail.com>
2017-07-03 04:29:30 -0500

    Fill out sound retrieval code

--- LCARS.CoreUi/Assets/Access/LcarsSound.cs
+++ LCARS.CoreUi/Assets/Access/LcarsSound.cs
@@ -1,36 +1,108 @@
-﻿using System.IO;
+﻿using LCARS.CoreUi.Helpers;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Media;
+using System.Reflection;
 
 namespace LCARS.CoreUi.Assets.Access
 {
-    class LcarsSound
+    public class LcarsSound
     {
+        private const string soundsPrefix = "LCARS.CoreUi.Assets.Sounds.";
+
         public static SoundPlayer soundPlayer = new SoundPlayer();
+
+        public static List<string> AlertWavs;
+        public static List<string> BeepWavs;
+        public static List<string> FailWavs;
+        public static List<string> ProcessingWavs;
+
+        static LcarsSound()
+        {
+            AlertWavs = new List<string>(GetResourceNamesFromNamespace(soundsPrefix + "Alert"));
+            BeepWavs = new List<string>(GetResourceNamesFromNamespace(soundsPrefix + "Beep"));
+            FailWavs = new List<string>(GetResourceNamesFromNamespace(soundsPrefix + "Fail"));
+            ProcessingWavs = new List<string>(GetResourceNamesFromNamespace(soundsPrefix + "Processing"));
+        }
+
         public static void Play(LcarsSoundAsset assetKey)
         {
-            var resourceStream = GetGetResourceStream(assetKey);
-            if (resourceStream == null) return;
+            var resourceSting = GetGetResourceString(assetKey);
+            if (resourceSting == null) return;
 
+            Play(resourceSting);
+        }
+
+        private static void Play(string resourceSting)
+        {
             lock (soundPlayer)
             {
-                soundPlayer.Stream = resourceStream;
-                soundPlayer.Play();
+                var thisAssembly = Assembly.GetExecutingAssembly();
+                using (Stream wavStream = thisAssembly.GetManifestResourceStream(resourceSting))
+                {
+                    soundPlayer.Stream = wavStream;
+                    soundPlayer.Play();
+                }
             }
         }
 
-        private static Stream GetGetResourceStream(LcarsSoundAsset assetKey)
+        private static string GetGetResourceString(LcarsSoundAsset assetKey)
         {
             switch (assetKey)
             {
+                case LcarsSoundAsset.RandomAlert:
+                    return AlertWavs.Random();
+                case LcarsSoundAsset.RandomBeep:
+                    return BeepWavs.Random();
+                case LcarsSoundAsset.RandomFail:
+                    return FailWavs.Random();
+                case LcarsSoundAsset.RandomProcessing:
+                    return ProcessingWavs.Random();
+
+                case LcarsSoundAsset.PlainBeep:
+                    return soundsPrefix + "Beep.004.wav";
+                case LcarsSoundAsset.PlainProcessing:
+                    return soundsPrefix + "Processing.001.wav";
+                case LcarsSoundAsset.TurnOn1:
+                    return soundsPrefix + "Toggle.003_turn_on.wav";
+                case LcarsSoundAsset.TurnOff1:
+                    return soundsPrefix + "Toggle.004_turn_off.wav";
+                case LcarsSoundAsset.TurnOn2:
+                    return soundsPrefix + "Toggle.001_doorbell_on.wav";
+                case LcarsSoundAsset.TurnOff2:
+                    return soundsPrefix + "Toggle.002_doorbell_off.wav";
+
                 case LcarsSoundAsset.Unset:
+                default:
                     return null;
             }
-            return null;
+        }
+
+        private static string[] GetResourceNamesFromNamespace(string nameSpace)
+        {
+            var executingAssembly = Assembly.GetExecutingAssembly();
+            return executingAssembly.GetManifestResourceNames()
+                .Where(r => r.StartsWith(nameSpace) && r.EndsWith(".wav"))
+                .ToArray();
         }
     }
 
     public enum LcarsSoundAsset
     {
         Unset = 0,
+
+        RandomAlert,
+        RandomBeep,
+        RandomFail,
+        RandomProcessing,
+
+        PlainBeep,
+        PlainProcessing,
+
+        TurnOn1,
+        TurnOff1,
+        TurnOn2,
+        TurnOff2,
     }
 }
---
+++ LCARS.CoreUi/Helpers/ListExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace LCARS.CoreUi.Helpers
+{
+    public static class List
+    {
+        public static T Random<T>(this List<T> thisList)
+        {
+            return thisList[Randomizer.NextInt(thisList.Count)];
+        }
+    }
+}
